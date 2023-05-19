365a9639a7fa21510a58658ebf14f823bb84bf02
JakubKrycki <kuba@krycki.pl>
2023-05-19 17:26:41 +0200

    Add temporary view of landpage and update landpage controller with getting the placemarks

--- src/controllers/landpage-controller.js
+++ src/controllers/landpage-controller.js
@@ -1,7 +1,15 @@
+import { db } from "../models/db.js";
+
 export const landpageController = {
     index: {
       handler: async function (request, h) {
-        return h.view("main");
+        const loggedInUser = request.auth.credentials;
+        const placemarks = await db.placemarkStore.getAllPlacemarksByUser(loggedInUser._id);
+        const viewData = {
+          user: loggedInUser,
+          placemarks: placemarks,
+        };
+        return h.view("landpage-view", viewData);
       },
     },
   };
