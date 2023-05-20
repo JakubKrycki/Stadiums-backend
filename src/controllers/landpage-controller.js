9d28fd9da83b82115308b604607cbbe3874c9bd6
JakubKrycki <kuba@krycki.pl>
2023-05-20 22:53:43 +0200

    Create basic table with placemarks and form to add new placemark

--- src/controllers/landpage-controller.js
+++ src/controllers/landpage-controller.js
@@ -12,4 +12,28 @@ export const landpageController = {
         return h.view("landpage-view", viewData);
       },
     },
+
+    addPlacemark: {
+      handler: async function (request, h) {
+        const loggedInUser = request.auth.credentials;
+        const newPlacemark = {
+          name: request.payload.name,
+          club: request.payload.club,
+          added_by: loggedInUser._id,
+          latitude: request.payload.latitude,
+          longitude: request.payload.longitude,
+          category: request.payload.category,
+          private: request.payload.private,
+        };
+        await db.placemarkStore.addPlacemark(newPlacemark);
+        return h.redirect("/landpage");
+      },
+    },
+  
+    deletePlacemark: {
+      handler: async function (request, h) {
+        await db.placemarkStore.deletePlacemarkById(request.params.id);
+        return h.redirect("/landpage");
+      },
+    },
   };
