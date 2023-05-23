3979acd6392305ce8cd067fff805d2e79fe07bcc
JakubKrycki <kuba@krycki.pl>
2023-05-23 17:55:51 +0200

    Add mongo database with seed-data

---
+++ src/models/mongo/seed-data.js
@@ -0,0 +1,24 @@
+export const seedData = {
+    users: {
+      _model: "User",
+      jakubKrycki: {
+        firstName: "Jakub",
+        lastName: "Krycki",
+        email: "jakub@krycki.pl",
+        password: "jk"
+      }
+    },
+    placemarks: {
+      _model: "Placemark",
+      stamfordBridge: {
+        name: "Stamford Bridge",
+        team: "Chelsea London F.C.",
+        added_by: "1",
+        latitude: "1",
+        longitude: "1",
+        category: "England",
+        private: true,
+      }
+    },
+  };
+  
